ad25603c606e73b4176adc3949e864c6b4e1c32e
Jorge Timoteo <jorge.timoteoprado@kellogg.northwestern.edu>
2023-05-18 07:57:58 +0000

    update2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ def create
   @post["title"] = params["post"]["title"]
   @post["posted_on"] = params["post"]["posted_on"]
   @post["description"] = params["post"]["description"]
+  @post["place_id"] = params["post"]["place_id"]
   @post.save
   redirect_to "/places/#{@post["place_id"]}"
 end
